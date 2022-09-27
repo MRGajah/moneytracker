7a53582e64521bdd142f74e243aa271ab5de55b8
Abanyaw <muhammadrizkigajah@gmail.com>
2022-09-27 16:44:28 +0700

    feat: add banner and my wallet card

---
+++ src/component/content/WalletItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WalletItems() {
+  return (
+    <div className='items'>
+        <div className="icon"></div>
+        <p className="wallet-title">Wallet Title</p>
+        <p className='amount'>Rp. 999,999,999</p>
+    </div>
+  )
+}
+
+export default WalletItems
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import Navbar from '../../component/navbar/Navbar'
 import Headbar from '../../component/headbar/Headbar'
+import WalletItems from '../../component/content/WalletItems'
 function home() {
   return (
     <div className='body'>
         <Headbar/>
-        home
+          <div className="container">
 
+            <div className="card banner content">
+              <div className="">
+                <p className='title'> My total income</p>
+                <p className='amount'>Rp. 999,999,9999</p>
+                <div className="mini-info">
+                  asdasdadasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                </div>
+              </div>
+            </div>
+
+            <div className="card content">
+              <div className="context-body">
+                <div className="title-box">
+                  <p className='title'>My Wallets</p>
+                  <p className='link-text'>See all</p>
+                </div>
+                <div className="dash-line"></div>
+                <div className="context-items">
+                  <WalletItems/>
+                  <WalletItems/>
+                  <WalletItems/>
+                </div>
+              </div>
+            </div>
+
+          </div>
         <Navbar/>
     </div>
   )
